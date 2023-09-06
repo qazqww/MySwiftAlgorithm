c3da962ad3aeb4c3f7fcbd366b205bc77f352103
raptarior <qazqww@naver.com>
2023-09-06 17:44:16 +0900

    230906

---
+++ MySwiftAlgorithm/BJAlgorithm/BOJ1193_FindFraction.swift
@@ -0,0 +1,26 @@
+//
+//  BOJ1193_FindFraction.swift
+//  MySwiftAlgorithm
+//
+//  Created by Jingon Lee on 2023/09/06.
+//
+
+import Foundation
+
+var N = Int(readLine()!)!
+var cnt = 0
+
+while N > 0 {
+    cnt += 1
+    N -= cnt
+}
+
+// cnt번째 대각선의 N번째 순서, cnt + 1은 분자 분모의 합
+N += cnt
+cnt += 1
+
+if cnt % 2 == 0 {
+    print("\(cnt - N)/\(N)")
+} else {
+    print("\(N)/\(cnt - N)")
+}
